d40aa17487a9226ffc361fc90515e1be111bd3c4
msosen <nananam608@gmail.com>
2018-05-08 15:23:25 +0200

    register view and form fixing started

--- home/forms.py
+++ home/forms.py
@@ -6,7 +6,7 @@ class RegisterForm(forms.Form):
     class MyDateInput(forms.DateInput): #inner class for input type date - otherwise render as text input
         input_type = 'date'
     numeric = RegexValidator(r'^\+?1?\d{9,15}$', "Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
-    firstName = forms.CharField(label="First name", max_length=30, widget=forms.TextInput(attrs={'placeholder': 'First name'}))
+    firstName = forms.CharField(label="First name", max_length=30, widget=forms.TextInput(attrs={'placeholder': 'First name', 'class': 'form-control'}))
     lastName = forms.CharField(label="Last name", max_length=50, widget=forms.TextInput(attrs={'placeholder': 'Last name'}))
     mail = forms.EmailField(label="Mail", max_length=100, widget=forms.EmailInput(attrs={'placeholder': "Mail address"}))
     city = forms.CharField(label="City", max_length=30, widget=forms.TextInput(attrs={'placeholder': 'City'}))
