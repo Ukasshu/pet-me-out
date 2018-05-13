58af519bf55f2da01bad8745a7c315295cdf40ef
Ukasshu <mielczareklukasz96@gmail.com>
2018-05-13 11:22:33 +0200

    'home' replaced with empty string

--- studio_projektowe/urls.py
+++ studio_projektowe/urls.py
@@ -19,7 +19,7 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home/', include('home.urls')),
+    path('', include('home.urls')),
     path('login/', auth_views.login, name='login'),
     path('logout/', auth_views.logout, name='logout'),
 ]
