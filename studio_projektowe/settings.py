5659f920c564b61f1f803aff1e5ef0ffde555051
Ukasshu <mielczareklukasz96@gmail.com>
2018-06-17 12:25:22 +0200

    profile upgrade

--- studio_projektowe/settings.py
+++ studio_projektowe/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'bootstrap3',
+    'jquery'
 ]
 
 MIDDLEWARE = [
@@ -115,5 +116,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'home/static')
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
